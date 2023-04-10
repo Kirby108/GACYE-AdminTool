bd3715efc850845a27a67ce5beeeca2c74ede9d6
Xing <mxing8@gatech.edu>
2023-04-10 03:06:51 -0400

    trimmed down components such as NFT, and RTL (Right-to-Left Admin) pages for scope.

--- frontend/horizon-ui-chakra-main/src/components/dataDispaly/TimelineRow.js
+++ frontend/horizon-ui-chakra-main/src/components/dataDispaly/TimelineRow.js
@@ -9,18 +9,6 @@ export default function TimelineRow(props) {
 	return (
 		<Flex alignItems='center' minH='78px' justifyContent='start' mb='5px'>
 			<Flex direction='column' h='100%'>
-				<Icon
-					as={logo}
-					bg={bgIconColor}
-					color={color}
-					h={'30px'}
-					w={'26px'}
-					pe='6px'
-					zIndex='1'
-					position='relative'
-					right={document.documentElement.dir === 'rtl' ? '-8px' : ''}
-					left={document.documentElement.dir === 'rtl' ? '' : '-8px'}
-				/>
 				<Box w='2px' bg='gray.200' h={index === arrLength - 1 ? '15px' : '100%'} />
 			</Flex>
 			<Flex direction='column' justifyContent='flex-start' h='100%'>
--- frontend/horizon-ui-chakra-main/src/views/admin/default/index.jsx
+++ frontend/horizon-ui-chakra-main/src/views/admin/default/index.jsx
@@ -47,7 +47,6 @@ import {
 import CheckTable from "views/admin/default/components/CheckTable";
 import ComplexTable from "views/admin/default/components/ComplexTable";
 import DailyTraffic from "views/admin/default/components/DailyTraffic";
-import PieCard from "views/admin/default/components/PieCard";
 import Tasks from "views/admin/default/components/Tasks";
 import TotalSpent from "views/admin/default/components/TotalSpent";
 import WeeklyRevenue from "views/admin/default/components/WeeklyRevenue";
@@ -154,7 +153,6 @@ export default function UserReports() {
         <CheckTable columnsData={columnsDataCheck} tableData={tableDataCheck} />
         <SimpleGrid columns={{ base: 1, md: 2, xl: 2 }} gap='20px'>
           <DailyTraffic />
-          <PieCard />
         </SimpleGrid>
       </SimpleGrid>
       <SimpleGrid columns={{ base: 1, md: 1, xl: 2 }} gap='20px' mb='20px'>

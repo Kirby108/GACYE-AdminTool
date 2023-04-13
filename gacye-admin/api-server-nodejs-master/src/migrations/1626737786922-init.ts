ca339ccdf6bc74589d5281e2460322e2f2570930
nabraham9 <nabraham9@gatech.edu>
2023-04-12 20:26:24 -0400

    queries work, reinstall deps

--- gacye-admin/api-server-nodejs-master/src/migrations/1626737786922-init.ts
+++ gacye-admin/api-server-nodejs-master/src/migrations/1626737786922-init.ts
@@ -4,11 +4,31 @@ export class init1626737786922 implements MigrationInterface {
     name = 'init1626737786922'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE "active_session" ("id" varchar PRIMARY KEY NOT NULL, "token" text NOT NULL, "userId" text NOT NULL, "date" datetime NOT NULL DEFAULT (CURRENT_TIMESTAMP))`);
-        await queryRunner.query(`CREATE TABLE "user" ("id" varchar PRIMARY KEY NOT NULL, "username" text NOT NULL, "email" text, "password" text, "user_role" varchar, "date" datetime NOT NULL DEFAULT (CURRENT_TIMESTAMP))`);
-        await queryRunner.query(`CREATE TABLE "role" ("id" varchar PRIMARY KEY NOT NULL, "name" text NOT NULL, "date" datetime NOT NULL DEFAULT (CURRENT_TIMESTAMP))`)
-        await queryRunner.query(`INSERT INTO role ("id", "name") VALUES(1, 'admin')`)
-        await queryRunner.query(`INSERT INTO role ("id", "name") VALUES(2, 'user')`)
+        await queryRunner.query('DROP TABLE IF EXISTS active_session')
+        await queryRunner.query(`CREATE TABLE active_session (
+            id varchar(40)  NOT NULL,
+            token text NOT NULL,
+            userId text NOT NULL,
+            date datetime NOT NULL DEFAULT (CURRENT_TIMESTAMP),
+            primary key (id))`);
+        await queryRunner.query('DROP TABLE IF EXISTS user')
+        await queryRunner.query(`CREATE TABLE user (
+            id varchar(40) NOT NULL, 
+            username text NOT NULL,
+            email text, 
+            password text, 
+            user_role varchar(40), 
+            date datetime NOT NULL DEFAULT (CURRENT_TIMESTAMP),
+            primary key (id))`);
+            await queryRunner.query('DROP TABLE IF EXISTS role')
+            await queryRunner.query(`CREATE TABLE role (
+                id varchar (40) NOT NULL, 
+                name text NOT NULL, 
+                date datetime NOT NULL DEFAULT (CURRENT_TIMESTAMP),
+                PRIMARY KEY (id)
+                ))`)
+        await queryRunner.query(`INSERT INTO role (id, name) VALUES(1, 'admin')`)
+        await queryRunner.query(`INSERT INTO role (id, name) VALUES(2, 'user')`)
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {

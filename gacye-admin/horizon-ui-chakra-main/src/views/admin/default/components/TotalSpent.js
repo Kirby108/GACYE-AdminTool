ea2dcaa826d1c25588f0e1b0bb8413659454e41b
nabraham9 <nabraham9@gatech.edu>
2023-04-14 19:41:55 -0400

    added maps to this branch

--- gacye-admin/horizon-ui-chakra-main/src/views/admin/default/components/TotalSpent.js
+++ gacye-admin/horizon-ui-chakra-main/src/views/admin/default/components/TotalSpent.js
@@ -11,19 +11,28 @@ import {
 import Card from "components/card/Card.js";
 import LineChart from "components/charts/LineChart";
 import React from "react";
-import { IoCheckmarkCircle } from "react-icons/io5";
-import { MdBarChart, MdOutlineCalendarToday } from "react-icons/md";
-// Assets
-import { RiArrowUpSFill } from "react-icons/ri";
-import {
-  lineChartDataTotalSpent,
-  lineChartOptionsTotalSpent,
-} from "variables/charts";
+import GoogleMapReact from "google-map-react";
+export default function TotalSpend(){
 
-export default function TotalSpent(props) {
-  const { ...rest } = props;
+  var dummyHeat = {
+    positions: [
+      {lat: 33.763396, lng: -84.392070},
+      {lat: 33.763396, lng: -84.392070},
+      {lat: 33.763396, lng: -84.392070},
+      {lat: 33.763396, lng: -84.392070}],
+    options: {   
+      radius: 20,   
+      opacity: 0.6
+    }
+  }
 
-  // Chakra Color Mode
+  const defaultProps = {
+    center: {
+      lat: 33.763396,
+      lng: -84.392070
+    },
+    zoom: 11
+  };
 
   const textColor = useColorModeValue("secondaryGray.900", "white");
   const textColorSecondary = useColorModeValue("secondaryGray.600", "white");
@@ -40,85 +49,21 @@ export default function TotalSpent(props) {
   );
   return (
     <Card
-      justifyContent='center'
-      align='center'
-      direction='column'
-      w='100%'
-      mb='0px'
-      {...rest}>
-      <Flex justify='space-between' ps='0px' pe='20px' pt='5px'>
-        <Flex align='center' w='100%'>
-          <Button
-            bg={boxBg}
-            fontSize='sm'
-            fontWeight='500'
-            color={textColorSecondary}
-            borderRadius='7px'>
-            <Icon
-              as={MdOutlineCalendarToday}
-              color={textColorSecondary}
-              me='4px'
-            />
-            This month
-          </Button>
-          <Button
-            ms='auto'
-            align='center'
-            justifyContent='center'
-            bg={bgButton}
-            _hover={bgHover}
-            _focus={bgFocus}
-            _active={bgFocus}
-            w='37px'
-            h='37px'
-            lineHeight='100%'
-            borderRadius='10px'
-            {...rest}>
-            <Icon as={MdBarChart} color={iconColor} w='24px' h='24px' />
-          </Button>
-        </Flex>
-      </Flex>
-      <Flex w='100%' flexDirection={{ base: "column", lg: "row" }}>
-        <Flex flexDirection='column' me='20px' mt='28px'>
-          <Text
-            color={textColor}
-            fontSize='34px'
-            textAlign='start'
-            fontWeight='700'
-            lineHeight='100%'>
-            $37.5K
-          </Text>
-          <Flex align='center' mb='20px'>
-            <Text
-              color='secondaryGray.600'
-              fontSize='sm'
-              fontWeight='500'
-              mt='4px'
-              me='12px'>
-              Total Spent
-            </Text>
-            <Flex align='center'>
-              <Icon as={RiArrowUpSFill} color='green.500' me='2px' mt='2px' />
-              <Text color='green.500' fontSize='sm' fontWeight='700'>
-                +2.45%
-              </Text>
-            </Flex>
-          </Flex>
-
-          <Flex align='center'>
-            <Icon as={IoCheckmarkCircle} color='green.500' me='4px' />
-            <Text color='green.500' fontSize='md' fontWeight='700'>
-              On track
-            </Text>
-          </Flex>
-        </Flex>
-        <Box minH='260px' minW='75%' mt='auto'>
-          <LineChart
-            chartData={lineChartDataTotalSpent}
-            chartOptions={lineChartOptionsTotalSpent}
-          />
-        </Box>
-      </Flex>
+        justifyContent='center'
+        align='center'
+        direction='column'
+        w='800px'
+        mb='0px'>
+    <div style={{ height: '100vh', width: '100%' }}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: "AIzaSyB6IN5uyLhb9KindBo0z_iVGgoFmY0ovCY" }}
+        defaultCenter={defaultProps.center}
+        defaultZoom={defaultProps.zoom}
+        heatmapLibrary={true}          
+        heatmap={dummyHeat} 
+      >
+      </GoogleMapReact>
+    </div>
     </Card>
   );
 }
